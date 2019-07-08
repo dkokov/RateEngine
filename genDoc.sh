c201876188c0dabd68283e8789117f84e0b440e5
dkokov <dkokov75@gmail.com>
2019-07-08 15:15:08 +0300

    add genDoc.sh

---
+++ genDoc.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DIR=`pwd`
+
+CONV=`which pandoc`
+
+# contert from md files to pdf file
+genDoc_pdf()
+{
+    $CONV README.md LICENSE.md doc/*.md --toc --number-sections --top-level-division=part -o rt_v7.pdf
+}
+
+# contert from md files to html file
+genDoc_html()
+{
+    $CONV README.md -o $tmp"/index.html"
+}
+
+if [ -f "$CONV" ];
+then
+    if [ $# -eq 0 ];
+    then
+	genDoc_pdf
+    else
+	if [ $1 == "html" ];
+	then
+#	    tmp=$DIR"/doc/html/"
+#	    mkdir $tmp
+#	    cp -vfR $DIR"/doc/png/" $tmp"/png/"
+	    genDoc_html
+	fi
+    fi
+else
+    echo "Sould be installed 'pandoc'"
+    exit
+fi
+
+#pandoc README.md  --toc --number-sections --top-level-division=part --output=rt_v7.pdf
+
