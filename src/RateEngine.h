b24267bd82e3ff5798f8deeeca54a8848f74e92c
Dimitar Kokov <dkokov75@gmail.com>
2023-05-22 12:44:31 +0300

    version 0.6.14.10(beta)

--- src/RateEngine.h
+++ src/RateEngine.h
@@ -1,5 +1,5 @@
 /* Application Info */
 #define VERSION "0.6"
-#define RELEASE "14.9(beta)"
-#define DATE_RELEASE  "2019-10-28"
+#define RELEASE "14.10(beta)"
+#define DATE_RELEASE  "2023-05-22"
 #define CREATOR "Dimitar Kokov"
