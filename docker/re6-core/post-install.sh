29de02ecf41efd5efa081ea77094b61201169742
dkokov <dkokov75@gmail.com>
2024-02-02 17:50:45 +0200

    removed static IP address from other files

--- docker/re6-core/post-install.sh
+++ docker/re6-core/post-install.sh
@@ -8,18 +8,17 @@ cd /re6-core/
 if [ -f /root/.pgpass ]; then
     echo "...keep on..."
 else
-#	mv -v /re6-core/tmp/ /usr/local/RateEngine
-	cp -vfR tmp/* /usr/local/RateEngine/
+#    mv -v /re6-core/tmp/ /usr/local/RateEngine
+    cp -vfR tmp/* /usr/local/RateEngine/
     cp -vf RateEngine6.xml /usr/local/RateEngine/config
     rm -fv /usr/local/RateEngine/config/cc_int/*
     cp -vf cc_int/* /usr/local/RateEngine/config/cc_int
     cp -vf .pgpass /root
     chmod 0600 /root/.pgpass
     sleep 5
-#    _RES_SQL=`$CLI -h 172.20.0.2 -U re_admin rate_engine -c "select date from version;" -A -t`
-	_RES_SQL=`$CLI -h re6-core -U re_admin rate_engine -c "select date from version;" -A -t`
+    _RES_SQL=`$CLI -h re6-db -U re_admin rate_engine -c "select date from version;" -A -t`
     if [ "$_RES_SQL" == "" ]; then 
-		$CLI -h 172.20.0.2 -U re_admin rate_engine -f $_SQL
+		$CLI -h re6-db -U re_admin rate_engine -f $_SQL
 	else
 		echo "the db was created before"
 	fi
