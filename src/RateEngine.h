79c29b276e02aaad8da1cb347e72493719d8c45f
dkokov <dkokov75@gmail.com>
2026-07-28 17:21:09 +0300

    modified:   RateEngine.h

--- src/RateEngine.h
+++ src/RateEngine.h
@@ -1,5 +1,5 @@
 /* Application Info */
 #define VERSION "0.7"
-#define RELEASE "5(devel)"
-#define DATE_RELEASE  "2026-07-13"
+#define RELEASE "6(devel)"
+#define DATE_RELEASE  "2026-07-28"
 #define CREATOR "Dimitar Kokov"
